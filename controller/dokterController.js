f14b5f05a34e6fe95e15c38d321270d7d90a8126
fjrdwc <fjrdwc@gmail.com>
2024-07-04 10:13:19 +0700

    update endpoint

--- controller/dokterController.js
+++ controller/dokterController.js
@@ -43,3 +43,101 @@ exports.getVeterinarians = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// Get detail of a veterinarian
+exports.getVeterinarianDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const veterinarian = await User.findByPk(id, {
+      include: {
+        model: Role,
+        attributes: ["id", "role_name"],
+      },
+      attributes: ["id", "nama", "no_telp", "email"],
+    });
+
+    if (!veterinarian) {
+      return res.status(404).json({ message: "Veterinarian not found" });
+    }
+
+    const formattedVeterinarian = {
+      id_user: veterinarian.id,
+      nama: veterinarian.nama,
+      role: {
+        id_role: veterinarian.Role.id,
+        role_name: veterinarian.Role.role_name,
+      },
+      no_telp: veterinarian.no_telp,
+      email: veterinarian.email,
+    };
+
+    res.status(200).json({
+      data: formattedVeterinarian,
+      message: "Get veterinarian detail success",
+      status: "1",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update veterinarian
+exports.updateVeterinarian = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nama, no_telp, email } = req.body;
+
+    const veterinarian = await User.findByPk(id);
+
+    if (!veterinarian) {
+      return res.status(404).json({ message: "Veterinarian not found" });
+    }
+
+    veterinarian.nama = nama || veterinarian.nama;
+    veterinarian.no_telp = no_telp || veterinarian.no_telp;
+    veterinarian.email = email || veterinarian.email;
+    await veterinarian.save();
+
+    const updatedVeterinarian = await User.findByPk(veterinarian.id, {
+      attributes: ["id", "nama", "no_telp", "email"],
+    });
+
+    const formattedVeterinarian = {
+      id_user: updatedVeterinarian.id,
+      nama: updatedVeterinarian.nama,
+      no_telp: updatedVeterinarian.no_telp,
+      email: updatedVeterinarian.email,
+    };
+
+    res.status(200).json({
+      data: formattedVeterinarian,
+      message: "Veterinarian updated successfully",
+      status: "1",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete veterinarian
+exports.deleteVeterinarian = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const veterinarian = await User.findByPk(id);
+
+    if (!veterinarian) {
+      return res.status(404).json({ message: "Veterinarian not found" });
+    }
+
+    await veterinarian.destroy();
+
+    res.status(200).json({
+      message: "Veterinarian deleted successfully",
+      status: "1",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/dokterRoutes.js
+++ routes/dokterRoutes.js
@@ -9,5 +9,20 @@ router.get(
   authenticationToken,
   dokterController.getVeterinarians
 );
+router.get(
+  "/veterinarian/:id",
+  authenticationToken,
+  dokterController.getVeterinarianDetail
+);
+router.put(
+  "/veterinarian/:id",
+  authenticationToken,
+  dokterController.updateVeterinarian
+);
+router.delete(
+  "/veterinarian/:id",
+  authenticationToken,
+  dokterController.deleteVeterinarian
+);
 
 module.exports = router;
